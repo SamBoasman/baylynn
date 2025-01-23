5165121ae76a38977fadd4f3b056494f85a6255b
Sam Boasman <samboasman@gmail.com>
2025-01-23 23:54:20 +1300

    Implement navbar side menu

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,27 +1,50 @@
-import logo from '../../assets/logo.webp';
 import './Navbar.scss';
+import React, { useState } from 'react';
+import logo from '../../assets/logo.webp';
 import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    document.body.style.overflow = menuOpen ? 'auto' : 'hidden';
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+    document.body.style.overflow = 'auto';
+  }
+
+  function NavLinks(name: string) {
+    return (
+      <div className={`nav-links ${name}`}>
+        <NavLink to='/' end>Home</NavLink>
+        <NavLink to='/AboutUs' end>About Us</NavLink>
+        <NavLink to='/Events' end>Events</NavLink>
+        <NavLink to='/SupportUs' end>Support Us</NavLink>
+        <NavLink to='/JoinUs' end>Join Us</NavLink>
+        <NavLink to='/Gallery' end>Gallery</NavLink>
+      </div>
+    )
+  }
+
   return (
     <header>
+        <div className={`overlay ${menuOpen ? 'open' : ''}`} onClick={closeMenu}></div>
+        <div className={`nav-menu ${menuOpen ? 'open' : ''}`}>
+          {NavLinks(`menu-links`)}
+        </div>
         <nav className='navbar'>
-            <div className='hamburger'>
-                <span className='bar'></span>
-                <span className='bar'></span>
-                <span className='bar'></span>
-            </div>
-            <Link className='nav-logo' to='/'>
-              <img className='nav-logo-img' src={logo}/>
-            </Link>
-            <div className='nav-headers'>
-                <NavLink to='/' end>Home</NavLink>
-                <NavLink to='/AboutUs' end>About Us</NavLink>
-                <NavLink to='/Events' end>Events</NavLink>
-                <NavLink to='/SupportUs' end>Support Us</NavLink>
-                <NavLink to='/JoinUs' end>Join Us</NavLink>
-                <NavLink to='/Gallery' end>Gallery</NavLink>
-            </div>
+          <button className='hamburger' onClick={toggleMenu}>
+              <span className='bar'></span>
+              <span className='bar'></span>
+              <span className='bar'></span>
+          </button>
+          <Link className='nav-logo' to='/'>
+            <img className='logo-img' src={logo}/>
+          </Link>
+          {NavLinks('nav-headers')}
         </nav>
     </header>
   );
