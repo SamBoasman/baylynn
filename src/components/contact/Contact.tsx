3c0b48a8d5a19a03b2ec3052afa787da2b66d991
Sam Boasman <samboasman@gmail.com>
2025-01-25 01:35:53 +1300

    Implement contact form

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import './Contact.scss';
+import React, { FormEvent } from "react";
+import styles from './Contact.module.scss';
 
 const Contact = () => {
+    const [result, setResult] = React.useState("");
+  
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.currentTarget);
+  
+      formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.currentTarget.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+  
     return (
-        <div>Contact Form</div>
+      <div className={styles['form-container']}>
+        <form onSubmit={onSubmit} className={styles['contact-form']}>
+          <input type="text" name="name" placeholder="Name" required/>
+          <input type="email" name="email" placeholder="Email" required/>
+          <textarea name="message" placeholder="Message" required></textarea>
+          <button type="submit">Submit Form</button>
+        </form>
+        <span>{result}</span>
+      </div>
     );
-}
+  }
 
 export default Contact;
